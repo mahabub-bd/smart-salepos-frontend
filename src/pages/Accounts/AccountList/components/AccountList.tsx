2299c779eac1491fcaf562518549feadaa9ebbe6
mahabub <mahabub@qpaybd.com.bd>
2025-11-25 09:25:08 +0600

    some fix

--- src/pages/Accounts/AccountList/components/AccountList.tsx
+++ src/pages/Accounts/AccountList/components/AccountList.tsx
@@ -63,100 +63,120 @@ export default function AccountListPage({
         <h2 className="text-lg font-semibold mb-3">
           {isCashBank ? "Cash & Bank Accounts" : "Chart of Accounts"}
         </h2>
-
-        <Table className="w-full text-sm">
-          <TableHeader className="border-b bg-gray-100 dark:bg-gray-700">
-            <TableRow>
-              <TableCell isHeader className="px-4 py-3 text-left">
-                Account Number
-              </TableCell>
-              <TableCell isHeader className="px-4 py-3 text-left">
-                Code
-              </TableCell>
-              <TableCell isHeader className="px-4 py-3 text-left">
-                Name
-              </TableCell>
-              <TableCell isHeader className="px-4 py-3 text-center">
-                Action
-              </TableCell>
-              <TableCell isHeader className="px-4 py-3 text-center">
-                Type
-              </TableCell>
-            </TableRow>
-          </TableHeader>
-
-          <TableBody>
-            {displayedAccounts.map((acc: any) => (
-              <TableRow key={acc.id} className="border-b last:border-none">
-                <TableCell className="px-4 py-2">
-                  {acc.account_number}
+        <div className="overflow-x-auto">
+          <Table>
+            <TableHeader className="border-gray-100 dark:border-gray-800 border-y">
+              <TableRow>
+                <TableCell
+                  isHeader
+                  className="px-4 py-3 text-left text-sm  font-medium text-gray-500 dark:text-gray-400"
+                >
+                  Account Number
                 </TableCell>
-                <TableCell className="px-4 py-2">{acc.code}</TableCell>
-                <TableCell className="px-4 py-2">{acc.name}</TableCell>
-
-                {/* Action Buttons */}
-                <TableCell className="px-4 py-2 flex justify-center gap-2">
-                  {acc.isCash && (
-                    <>
-                      <IconButton
-                        icon={DollarSign}
-                        color="blue"
-                        size={16}
-                        tooltip="Add Cash"
-                        onClick={() => openModal(acc, "cash")}
-                      />
-                      <IconButton
-                        icon={ArrowRightLeft}
-                        color="purple"
-                        size={16}
-                        tooltip="Fund Transfer"
-                        onClick={() => openModal(acc, "transfer")}
-                      />
-                    </>
-                  )}
-
-                  {acc.isBank && (
-                    <>
-                      <IconButton
-                        icon={PlusCircle}
-                        color="green"
-                        size={16}
-                        tooltip="Add Balance"
-                        onClick={() => openModal(acc, "bank")}
-                      />
-                      <IconButton
-                        icon={Shuffle}
-                        size={16}
-                        tooltip="Fund Transfer"
-                        onClick={() => openModal(acc, "transfer")}
-                        className="bg-orange-500 hover:bg-orange-600 text-white"
-                      />
-                    </>
-                  )}
+                <TableCell
+                  isHeader
+                  className="px-4 py-3 text-left text-theme-xs font-medium text-gray-500 dark:text-gray-400"
+                >
+                  Code
                 </TableCell>
-
-                {/* Account Type Badge */}
-                <TableCell className="px-4 py-2 text-center">
-                  <AccountBadge
-                    color={
-                      acc.type === "asset"
-                        ? "blue"
-                        : acc.type === "liability"
-                        ? "orange"
-                        : acc.type === "equity"
-                        ? "purple"
-                        : acc.type === "income"
-                        ? "green"
-                        : "red"
-                    }
-                  >
-                    {acc.type}
-                  </AccountBadge>
+                <TableCell
+                  isHeader
+                  className="px-4 py-3 text-left text-theme-xs font-medium text-gray-500 dark:text-gray-400"
+                >
+                  Name
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-4 py-3 text-left text-theme-xs font-medium text-gray-500 dark:text-gray-400"
+                >
+                  Action
+                </TableCell>
+                <TableCell
+                  isHeader
+                  className="px-4 py-3 text-left text-theme-xs font-medium text-gray-500 dark:text-gray-400"
+                >
+                  Type
                 </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+
+            <TableBody className="divide-y divide-gray-100 dark:divide-gray-800 mx-auto">
+              {displayedAccounts.map((acc: any) => (
+                <TableRow key={acc.id} className="border-b last:border-none">
+                  <TableCell className="px-4 py-2">
+                    {acc.account_number}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-sm text-left text-gray-800 dark:text-gray-100">
+                    {acc.code}
+                  </TableCell>
+                  <TableCell className="px-4 py-3 text-sm text-left text-gray-800 dark:text-gray-100">
+                    {acc.name}
+                  </TableCell>
+
+                  {/* Action Buttons */}
+                  <TableCell className="px-4 py-2 flex justify-center gap-2">
+                    {acc.isCash && (
+                      <>
+                        <IconButton
+                          icon={DollarSign}
+                          color="blue"
+                          size={16}
+                          tooltip="Add Cash"
+                          onClick={() => openModal(acc, "cash")}
+                        />
+                        <IconButton
+                          icon={ArrowRightLeft}
+                          color="purple"
+                          size={16}
+                          tooltip="Fund Transfer"
+                          onClick={() => openModal(acc, "transfer")}
+                        />
+                      </>
+                    )}
+
+                    {acc.isBank && (
+                      <>
+                        <IconButton
+                          icon={PlusCircle}
+                          color="green"
+                          size={16}
+                          tooltip="Add Balance"
+                          onClick={() => openModal(acc, "bank")}
+                        />
+                        <IconButton
+                          icon={Shuffle}
+                          size={16}
+                          tooltip="Fund Transfer"
+                          onClick={() => openModal(acc, "transfer")}
+                          className="bg-orange-500 hover:bg-orange-600 text-white"
+                        />
+                      </>
+                    )}
+                  </TableCell>
+
+                  {/* Account Type Badge */}
+                  <TableCell className="px-4 py-2 text-center">
+                    <AccountBadge
+                      color={
+                        acc.type === "asset"
+                          ? "blue"
+                          : acc.type === "liability"
+                          ? "orange"
+                          : acc.type === "equity"
+                          ? "purple"
+                          : acc.type === "income"
+                          ? "green"
+                          : "red"
+                      }
+                    >
+                      {acc.type}
+                    </AccountBadge>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       </div>
 
       {/* 🔹 Modal Manager */}
